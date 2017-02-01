814a13bba392099f34aa1b1a32f86154a34e9333
Immo Landwerth <immol@microsoft.com>
2017-01-31 19:32:50 -0800

    Initial commit

---
+++ ClassLibrary_NetStandard20/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static IEnumerable<int> CreateData()
+        {
+            return new Bag<int> { 1, 2, 3 };
+        }
+    }
+}
---
+++ ConsoleApp_NetCore20/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var items = ClassLibrary1.Class1.CreateData();
+        foreach (var item in items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
